f51772085202c62ab84c8185affda3daa48989e8
TejaSreenivas <teja.sreenivas.05@gmail.com>
2018-09-17 17:28:08 +0530

    following added
    batch norm at input
    new hidden layer
    validation dataset upate

--- cnn_model.py
+++ cnn_model.py
@@ -23,6 +23,7 @@ class model:
 	"""
 	def cnn_2d(self,data,is_train,prob_keep,param):
 		with tf.variable_scope("conv_1",reuse = tf.AUTO_REUSE):
+			data = tf.layers.batch_normalization(data,training=is_train)
 			conv1 = create_conv_layer(data,[ param[1][0], param[1][0], param[0], param[1][1] ], [1]*4, "SAME")
 			conv1 = tf.layers.dropout(conv1,rate=0.4,training=is_train)
 			conv1 = tf.nn.max_pool(conv1,ksize = [1,2,2,1],strides=[1,2,2,1],padding='SAME')
