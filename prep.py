8f848807b36c0c548738f8cb5a07ac151522ab3a
TejaSreenivas <teja.sreenivas.05@gmail.com>
2018-09-29 18:18:33 +0530

    introduced data augmentation

--- prep.py
+++ prep.py
@@ -1,8 +1,10 @@
 import numpy as np
 import os
 import load_mnist
-
-
+import tensorflow as tf
+from math import ceil, floor
+IMAGE_SIZE = 28
+channels = 1
 def reshape_image(x):
     x = np.transpose(x,[0,2,3,1])
     return x
@@ -47,7 +49,69 @@ def merger():
 	return merged
 
 
-	
+
+def get_translate_parameters(index):
+    if index == 0: # Translate left 20 percent
+        offset = np.array([0.0, 0.2], dtype = np.float32)
+        size = np.array([IMAGE_SIZE, ceil(0.8 * IMAGE_SIZE)], dtype = np.int32)
+        w_start = 0
+        w_end = int(ceil(0.8 * IMAGE_SIZE))
+        h_start = 0
+        h_end = IMAGE_SIZE
+    elif index == 1: # Translate right 20 percent
+        offset = np.array([0.0, -0.2], dtype = np.float32)
+        size = np.array([IMAGE_SIZE, ceil(0.8 * IMAGE_SIZE)], dtype = np.int32)
+        w_start = int(floor((1 - 0.8) * IMAGE_SIZE))
+        w_end = IMAGE_SIZE
+        h_start = 0
+        h_end = IMAGE_SIZE
+    elif index == 2: # Translate top 20 percent
+        offset = np.array([0.2, 0.0], dtype = np.float32)
+        size = np.array([ceil(0.8 * IMAGE_SIZE), IMAGE_SIZE], dtype = np.int32)
+        w_start = 0
+        w_end = IMAGE_SIZE
+        h_start = 0
+        h_end = int(ceil(0.8 * IMAGE_SIZE)) 
+    else: # Translate bottom 20 percent
+        offset = np.array([-0.2, 0.0], dtype = np.float32)
+        size = np.array([ceil(0.8 * IMAGE_SIZE), IMAGE_SIZE], dtype = np.int32)
+        w_start = 0
+        w_end = IMAGE_SIZE
+        h_start = int(floor((1 - 0.8) * IMAGE_SIZE))
+        h_end = IMAGE_SIZE 
+        
+    return offset, size, w_start, w_end, h_start, h_end
+
+def translate_images(X_imgs):
+    offsets = np.zeros((len(X_imgs), 2), dtype = np.float32)
+    n_translations = 4
+    X_translated_arr = []
+    
+    tf.reset_default_graph()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(n_translations):
+            X_translated = np.zeros((len(X_imgs), IMAGE_SIZE, IMAGE_SIZE, channels), dtype = np.float32)
+            X_translated.fill(0.0) # Filling background color
+            base_offset, size, w_start, w_end, h_start, h_end = get_translate_parameters(i)
+            offsets[:, :] = base_offset 
+            glimpses = tf.image.extract_glimpse(X_imgs, size, offsets)
+            
+            glimpses = sess.run(glimpses)
+            X_translated[:, h_start: h_start + size[0], w_start: w_start + size[1], :] = glimpses
+            X_translated_arr.extend(X_translated)
+    X_translated_arr = np.array(X_translated_arr, dtype = np.float32)
+    return X_translated_arr
+
+def mnist_augmented_data():
+	mnist = mnist_funct()
+	translated_imgs = translate_images(mnist['train_x'])
+	translated_imgs = np.concatenate([mnist['train_x'],translated_imgs],axis=0).reshape((-1,28,28,1))
+	translated_labels = np.concatenate([mnist['train_y'] for _ in range(5)],axis=0)
+	translated_labels = translated_labels.reshape((-1,))
+	mnist['train_x'] = translated_imgs
+	mnist['train_y'] = translated_labels
+	return mnist
 	
 
 
